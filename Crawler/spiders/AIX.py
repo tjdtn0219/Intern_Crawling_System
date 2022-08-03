93069bc5b20d0a23b2e09d2d27e521fd7fd356bb
tjdtn0219 <s_sjang@naver.com>
2022-08-03 04:01:51 +0000

    AIX MODIFICATION

--- Crawler/spiders/AIX.py
+++ Crawler/spiders/AIX.py
@@ -12,7 +12,7 @@ class AIXSpider(scrapy.Spider):
         items = response.xpath('//*[@id="mw-content-text"]/div[1]/table[4]/tbody/tr').getall()
         result = dict()
         for item in items:
-            if Selector(text=item).xpath('.//td').get():
+            if Selector(text=item).xpath('.//td/@data-sort-value').get():
                 result['Version'] = Selector(text=item).xpath('.//td/@data-sort-value').get().strip()
                 result['Date'] = Selector(text=item).xpath('.//td[2]/text()').get().strip()
                 yield result
